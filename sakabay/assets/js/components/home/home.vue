06a929518df69f7c7abebeab68a98159a29c5787
Andreas <andreasadelson@gmail.com>
2020-08-20 16:44:50 +0200

    test wamp

--- sakabay/assets/js/components/home/home.vue
+++ sakabay/assets/js/components/home/home.vue
@@ -1,22 +1,31 @@
 <template>
-    <b-container fluid class="p-4">
-      <b-row>
-        <b-col>
-            <b-img :src="images.default" center alt="Center image"></b-img>
+  <b-container
+    fluid
+    class="p-4"
+  >
+    <b-row>
+      <b-col>
+        <b-img
+          :src="images.default"
+          center
+          alt="Center image"
+        ></b-img>
+        <p>toto</p>
+        <h1>Sakabay</h1>
 
-          <!-- <img :src="images.logo" alt="logo"> -->
-        </b-col>
-      </b-row>
-    </b-container>
+        <!-- <img :src="images.logo" alt="logo"> -->
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 
 <script>
 import setAreaHeightMixin from 'mixins/setAreaHeightMixin';
 export default {
-  data(){
-    return{
-        images:  require('../../../pictures/logo.png')
+  data () {
+    return {
+      images: require('../../../pictures/logo.png')
     }
   },
   components: {
--- utils/encore_watch.sh
+++ utils/encore_watch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
-docker-compose restart &
+
 ${dir}/../sakabay/node_modules/.bin/encore dev --watch --watch-poll=1000
